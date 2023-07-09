88c8756bfdd1411122df009396cce0631293c823
MonicaIzzo <info@monicaizzo.net>
2023-07-09 22:55:02 +0200

    Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,11 @@ import axios from 'axios';
 import { api } from './data';
 import { store } from './data/store';
 import SearchForm from './components/SearchForm.vue';
+import ProductionCard from './components/ProductionCard.vue';
 
 export default {
     name: 'Boolflix',
-    components: { SearchForm },
+    components: { SearchForm, ProductionCard },
     data() {
         return {
             store,
@@ -60,21 +61,11 @@ export default {
     <SearchForm @term-change="setTitleFilter" @form-submit="searchProductions" />
     <section id="movies">
         <h2>Movies</h2>
-        <ul v-for="movie in store.movies" :key="movie.id">
-            <li>{{ movie.title }}</li>
-            <li>{{ movie.original_title }}</li>
-            <li>{{ movie.original_language }}</li>
-            <li>{{ movie.vote_average }}</li>
-        </ul>
+        <ProductionCard v-for="movie in store.movies" :key="movie.id" :item="movie" />
     </section>
     <section id="series">
         <h2>Series</h2>
-        <ul v-for="serie in store.series" :key="movie.id">
-            <li>{{ serie.name }}</li>
-            <li>{{ serie.original_name }}</li>
-            <li>{{ serie.original_language }}</li>
-            <li>{{ serie.vote_average }}</li>
-        </ul>
+        <ProductionCard v-for="serie in store.series" :key="serie.id" :item="serie" />
     </section>
 </template>
 
